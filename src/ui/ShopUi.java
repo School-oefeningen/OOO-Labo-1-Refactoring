13c1f3ca2005f32ebb0010de85522dfe8dae2c58
Robbe Delarbre <robbe.delarbre@student.ucll.be>
2020-09-24 13:41:39 +0200

    test

--- src/ui/ShopUi.java
+++ src/ui/ShopUi.java
@@ -6,6 +6,7 @@ import javax.swing.*;
 
 public class ShopUi {
     public static void main(String[] args) {
+        //test
         Shop shop = new Shop();
 
         String menu = "1. Add product\n2. Show product\n3. Show rental price\n\n0. Quit";
